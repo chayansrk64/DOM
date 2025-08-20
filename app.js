bd04e72f47a5ba3b69a9204f60fd3adf1346a5c6
chayansrk64 <chayansrk64@gmail.com>
2025-08-20 17:41:18 +0600

    created element

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const myQuerySelector2 = document.querySelector("#city p span");
   const getIdAttribute = para1.getAttribute("id");
   const getClassAttribute = para1.getAttribute("class");
   const setAnAttribute = para1.setAttribute("title", "tooltip by js");
-  console.log(getClassAttribute);
+//   console.log(getClassAttribute);
 }
 
 function interAction() {
@@ -53,5 +53,59 @@ function interAction() {
   btn.style.border = "none";
 }
 
+function showLink() {
+  const anchor = document.getElementById("anchor");
+  const href = anchor.getAttribute("href");
+  document.getElementById("show-link").innerText = href;
+}
+
+// change image
+{
+  function changeImage() {
+    const image = document.getElementById("image");
+    image.setAttribute('src', 'https://images.unsplash.com/photo-1755479595813-e7dca32937cb?')
+    
+  }
+}
 
 
+{
+  const nodes = document.getElementById('city');
+  // console.log(nodes.childNodes[3].childNodes[1].childNodes[0].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode)
+}
+ 
+// append child
+{
+   const ul = document.getElementById('city-list');
+   const li = document.createElement('li');
+   li.innerText = "Barisal";
+   ul.appendChild(li)
+}
+
+
+
+
+// parent node;
+const main = document.getElementById('main');
+// create a child node;
+const section = document.createElement('section')
+const ul = document.createElement('ul')
+
+const li1 = document.createElement('li');
+li1.innerText = "list item one!";
+ul.appendChild(li1)
+
+const li2 = document.createElement('li');
+li2.innerText = "list item two!";
+ul.appendChild(li2)
+
+const li3 = document.createElement('li');
+li3.innerText = "list item three!";
+ul.appendChild(li3)
+
+section.appendChild(ul)
+main.appendChild(section)
+
+
+
+// easiest way to create and append element 
